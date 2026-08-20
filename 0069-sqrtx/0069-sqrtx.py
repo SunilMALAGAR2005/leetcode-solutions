8fea899d93e1a6931804daf389d1f6347d3677cc
SunilMALAGAR2005 <sunilmalagar09@gmail.com>
2026-08-20 18:17:31 +0530

    Time: 2 ms (74.66%), Space: 19.4 MB (5.79%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l=1
+        r=x
+        while l<=r:
+            m=(r-l)//2+l
+            if m>x/m:
+                r=m-1
+            elif m<x/m:
+                l=m+1
+            else:
+                return m
+        return r
+        
